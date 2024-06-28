30d62e2361d43af0569dbb3fe03acfb615ed0d9c
Anju Sunny <anjusunnythomas@gmail.com>
2024-06-28 13:09:26 +0400

    initial commit

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from '../Home/Home'
+import About from '../About/About'
+
+const Layout = () => {
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+    </Routes>
+    </BrowserRouter>
+
+    </>
+  )
+}
+
+export default Layout
